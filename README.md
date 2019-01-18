aaae70512cb23a859e4ab4db8f523d08422e8b14
jrudascas <jrudascas@gmail.com>
2019-01-18 17:44:25 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+SPM Standalone
+- https://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/spm12_r7487.zip
+
+
+MATLAB Runtime
+- https://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/MCR/
+
+See the following link for installation details:
+
+- https://en.wikibooks.org/wiki/SPM/Standalone
---
+++ to_raw_files.py
@@ -0,0 +1,18 @@
+import os
+
+folder_path = '/home/brainlab/Desktop/Rudas/Data/Propofol_Movie/'
+
+cont = 1
+for (root, dirs, files) in os.walk(folder_path):
+    for file in sorted(files):
+        if not file.endswith('.nii'):
+            try:
+                os.remove(root + '/' + file)
+            except Exception:
+                continue
+        else:
+            if not file.startswith('f') and not file.startswith('s'):
+                try:
+                    os.remove(root + '/' + file)
+                except Exception:
+                    continue
